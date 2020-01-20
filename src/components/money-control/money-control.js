473d9a3df649b5408a913465eff83046182336a3
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-20 18:26:04 +0100

    finished section 2

--- src/components/money-control/money-control.js
+++ src/components/money-control/money-control.js
@@ -1,11 +1,69 @@
 import React from "react";
+import { Waypoint } from 'react-waypoint';
 
 import "./money-control.scss";
 
-export default () =>
-  <section className="money-control">
-    Total control over your money
-    <img src="money-control.svg" alt="money-control" />
-    <img className="trapeze" src="trapeze-money-control.svg" alt="bg-trapeze" />
-  </section>
-  
+const MoneyControl = () => {
+  const handleWaypointEnter = (div) => {
+    const el = document.querySelector(div);
+    el.classList.add("transition-on");
+  }
+
+  const handleWaypointLeave = (div) => {
+    const el = document.querySelector(div);
+    el.classList.remove("transition-on");
+  }
+
+  return (
+    <section className="money-control">
+      <div className="money-control-left">
+        <Waypoint
+          onEnter={() => handleWaypointEnter(".money-control-img")}
+          onLeave={() => handleWaypointLeave(".money-control-img")}
+        >
+         <img className="money-control-img" src="money-control.svg" alt="money-control" />
+        </Waypoint>
+      </div>
+
+      <Waypoint
+        onEnter={() => handleWaypointEnter(".money-control-right")}
+        onLeave={() => handleWaypointLeave(".money-control-right")}
+      >
+        <div className="money-control-right">
+          <h2>Total <span className="green-text">control</span> over your <span className="green-text">money</span></h2>
+
+          <div className="list-item">
+            <div className="dot purple-dot-key" />
+            <p>Overview of your finances</p>
+          </div>
+          <div className="list-item">
+            <div className="dot purple-dot-key" />
+            <p>Unlimited transactions history</p>
+          </div>
+          <div className="list-item">
+            <div className="dot purple-dot-key" />
+            <p>Get real time notifications</p>
+          </div>
+
+          <div className="conclusion">
+            <p>
+              With Zest, banking, bookkeeping 
+              and invoicing are all in one place. 
+              You’ll always know where you stand.
+            </p>
+          </div>
+        </div>
+      </Waypoint>
+
+      <div className="dot green-dot-left" />
+      <div className="dot green-dot-right" />
+      <div className="dot grey-dot-left" />
+      <div className="dot grey-dot-center" />
+      <img className="background-circle-bottom" src="curve-1.svg" alt="background-circle" />
+      <img className="trapeze" src="trapeze-money-control.svg" alt="bg-trapeze" />
+    </section>
+  );
+};
+
+export default MoneyControl;
+
