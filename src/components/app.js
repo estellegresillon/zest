761bb8c27bcc9f401933af556f7aad1b472d5ea4
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-20 09:20:31 +0100

    created one page layout

---
+++ src/components/app.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './app.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        Zest
+      </header>
+      <section className="hero-scene">
+        Smart banking for freelances
+      </section>
+      <section className="money-control">
+        Total control over your money
+      </section>
+      <div className="lined-bg">
+        <div className="one" />
+        <div className="two" />
+        <div className="three" />
+        <div className="four" />
+        <div className="five" />
+        <div className="six" />
+      </div>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.scss';
-import App from './App';
+import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('zest'));
