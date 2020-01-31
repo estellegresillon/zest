8546fad43189392a24ce0acd2ed59bbf7665dcc7
Estelle Grésillon <estelle.gresillon@gmail.com>
2020-01-31 15:56:51 +0100

    Updates

---
+++ precache-manifest.c17e5ddbc7da5a1a24c37c7851d48c60.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21be6de5401f38fceec7e5b2d1dcca39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1151557d37b97f21c2cf",
+    "url": "/static/css/main.83ca6ba9.chunk.css"
+  },
+  {
+    "revision": "031f5ab493da0968b9cb",
+    "url": "/static/js/2.43d84601.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.43d84601.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1151557d37b97f21c2cf",
+    "url": "/static/js/main.c9529d63.chunk.js"
+  },
+  {
+    "revision": "f12ed8445672a9985439",
+    "url": "/static/js/runtime-main.275e0d8c.js"
+  },
+  {
+    "revision": "6ffb302ad1948f8dcb9dbab54387275c",
+    "url": "/static/media/Axiforma-Book.6ffb302a.woff"
+  },
+  {
+    "revision": "b4afda971855a50bfaaf56e6cd6b1f85",
+    "url": "/static/media/Axiforma-Regular.b4afda97.woff"
+  }
+]);
